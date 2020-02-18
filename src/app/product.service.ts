37c534790ff4af8afbeeb8a3b7407fb61a06a941
Andrei Tudor <andrei.tudor@ivanti.com>
2020-02-18 16:16:01 +0200

    This is crap, I do have a _productsUrl property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Album} from './album';
+import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
 import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
@@ -20,6 +20,8 @@ export class ProductService {
   }
 
   public getProducts(): Observable<Product[]> {
-    return this._http.get(this._productUrl).map(response => <Product[]> response.json());
+    return this._http
+      .get(this._productsUrl)
+      .map(response => <Product[]>response.json());
   }
 }
