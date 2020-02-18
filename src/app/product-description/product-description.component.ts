0be5f817635919d2ef2e6c60d6faffb5f24015c8
Andrei Tudor <andrei.tudor@ivanti.com>
2020-02-18 13:09:27 +0200

    Remove access modifier from albumInfo

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,14 +7,12 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  public albumInfo: any;
+  albumInfo: any;
 
   constructor(private _productService: ProductService) {
   }
 
   ngOnInit() {
-    this._productService
-      .getAlbum(1)
-      .subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 }
